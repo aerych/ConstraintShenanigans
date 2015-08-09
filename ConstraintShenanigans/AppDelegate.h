55d5b96722c49c1bf51daa365598a8308426dade
aerych <aerychj@gmail.com>
2015-08-09 17:04:37 -0500

    Initial Commit

---
+++ ConstraintShenanigans/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConstraintShenanigans
+//
+//  Created by aerych on 8/9/15.
+//  Copyright (c) 2015 aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConstraintShenanigans/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConstraintShenanigans
+//
+//  Created by aerych on 8/9/15.
+//  Copyright (c) 2015 aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
